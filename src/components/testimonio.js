d2df8bfc608eab2daefeb32beff375cafd5c588c
Jose Contreras <josedanielcd@hotmail.com>
2022-10-23 14:29:20 -0500

    first commit

---
+++ src/components/testimonio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Textimonio() {
+  return (
+    <div className = "contenedor-testimonio">
+      <img className = "imagen-testimonio" 
+      src={require("../images/Emma.png")} 
+      alt="Emma photo"/>
+    </div>
+  );
+}
